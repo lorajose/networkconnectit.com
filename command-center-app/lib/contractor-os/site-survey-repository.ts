9a41619274792531fd24c8d8f0fd09a1ff6afcc4
joselora <jsloravargas@gmail.com>
2026-09-21 23:52:35 -0400

    NCI-074: import survey point taxonomy for server validation

--- command-center-app/lib/contractor-os/site-survey-repository.ts
+++ command-center-app/lib/contractor-os/site-survey-repository.ts
@@ -4,7 +4,7 @@ import { Prisma } from "@prisma/client";
 import { prisma } from "@/lib/db";
 import type { CommercialActor } from "./commercial-access";
 import { commercialReadScope, requireCommercialWriteAccess } from "./commercial-access";
-import { checklistForDisciplines, type SurveyDiscipline } from "./site-survey";
+import { checklistForDisciplines, SURVEY_POINT_TYPES, type SurveyDiscipline } from "./site-survey";
 
 export type SurveyAssignmentSummary = {
   id: string;
